6305f19db72fb74dd0208e6d9f1138970e8787de
git <git@github.com>
2021-06-23 01:41:30 +0100

    updated styling for list of most visited sites and limited to 5

--- src/Components/TopSites/TopSites.js
+++ src/Components/TopSites/TopSites.js
@@ -1,28 +1,21 @@
 /*global chrome*/
 import React, { Component } from "react";
+import './TopSites.scss';
 
 export class TopSites extends Component {
   constructor(props) {
     super(props);
-    // this.onAnchorClick = this.onAnchorClick.bind(this);
     this.buildPopupDom = this.buildPopupDom.bind(this);
   }
 
-  // onAnchorClick(event) {
-  //   chrome.tabs.create({ url: event.srcElement.href });
-  //   return false;
-  // }
-
   buildPopupDom(mostVisitedURLs) {
     var popupDiv = document.getElementById("mostVisited_div");
-    var ol = popupDiv.appendChild(document.createElement("ol"));
+    var ul = popupDiv.appendChild(document.createElement("ul"));
 
-    for (var i = 0; i < mostVisitedURLs.length; i++) {
-      var li = ol.appendChild(document.createElement("li"));
-      var a = li.appendChild(document.createElement("a"));
-      a.href = mostVisitedURLs[i].url;
-      a.appendChild(document.createTextNode(mostVisitedURLs[i].title));
-      // a.addEventListener("click", this.onAnchorClick);
+    for (var i = 0; i < 5; i++) {
+
+      var li = ul.appendChild(document.createElement("li"));
+      li.appendChild(document.createTextNode(mostVisitedURLs[i].title));
     }
   }
 
